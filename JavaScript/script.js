19895dc1d5d4c7ac8ffb7a1a1084fda69375319b
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-10 13:39:02 +0530

    Portfolio

---
+++ JavaScript/script.js
@@ -0,0 +1,45 @@
+//Auto typed text
+var typed = new Typed(".auto-input", {
+    strings: ["Soumya Prakash Sahu", "Full Stack Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});
+
+//For Navbar Scroller
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+};
+
+//For Navbar Hide on click
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+});
+
+//For Contact input box
+const inputs = document.querySelectorAll(".input");
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
