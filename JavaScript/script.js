5a75d2a9916f5df960fab90082f70c48cd05fba3
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-11 09:46:53 +0530

    confirm

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -43,3 +43,21 @@ inputs.forEach((input) => {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);
 });
+
+//Emailjs Function
+const btn = document.getElementById('send_message_btn');
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    btn.value = 'Sending...';
+    const serviceID = 'default_service';
+    const templateID = 'template_3oj9jzg';
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Sent!');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+});
